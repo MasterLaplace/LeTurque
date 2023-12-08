a631b92f562379d120fc14b4c690a45b103f366d
Andrea-Mancion <andrea.mancion@epitech.eu>
2023-12-08 04:16:28 +0100

    feat(EasterEgg): add a new Easter egg when we found a button

--- static/js/script.js
+++ static/js/script.js
@@ -36,3 +36,12 @@ function generateText() {
         document.getElementById("submitButton").disabled = false;
     });
 }
+
+function showContent() {
+    const hiddenButton = document.getElementById("hideButton");
+    hiddenButton.style.display = "block";
+}
+
+function redirectURL() {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
